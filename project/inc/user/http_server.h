46bfa95c07c07c797da89ba6d56a50b74fe35fc7
pvvx <pvvx@mail.ru>
2017-02-22 04:48:31 +0300

    add

---
+++ project/inc/user/http_server.h
@@ -0,0 +1,15 @@
+/*
+ * http_server.h
+ *
+ *  Created on: Feb 20, 2017
+ *      Author: user003
+ */
+
+#ifndef HTTP_SERVER_H_
+#define HTTP_SERVER_H_
+
+
+#define FLASH_APP_BASE  0xd0000
+void httpd_start(void);
+
+#endif /* HTTP_SERVER_H_ */
